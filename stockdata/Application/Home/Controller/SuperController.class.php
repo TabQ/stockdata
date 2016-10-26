96260c7290c5405da1ade1e302f9fa8f793febce
TabQ <evenstar6@gmail.com>
2016-10-26 19:27:26 +0800

    修正超涨/跌按today统计的不当之处

--- stockdata/Application/Home/Controller/SuperController.class.php
+++ stockdata/Application/Home/Controller/SuperController.class.php
@@ -9,18 +9,18 @@ class SuperController extends Controller {
 
     protected function _initialize() {
         $this->today = C('TODAY');
-//        $this->today = '2016-08-01';
+//        $this->today = '2016-09-19';
     }
 
     public function super_down() {
-        $count = M('super_wave')->where("date='$this->today' and direction=-1")->count();
+        $count = M('super_wave')->where("direction=-1")->count();
         $page = new \Think\Page($count, PAGE_COUNT);
         $show = $page->show();
 
         $list = M('super_wave')
-            ->where("date = '$this->today' and direction=-1")
+            ->where("direction=-1")
             ->join('left join stocks_info on super_wave.code = stocks_info.code')
-            ->field('super_wave.id, super_wave.code, name, industry, min, max, percent, cur_per')
+            ->field('super_wave.id, super_wave.code, date, man_date, name, industry, min, max, percent, cur_per, flag, cost_price, yield_rate')
             ->order('percent')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
@@ -33,14 +33,14 @@ class SuperController extends Controller {
     }
 
     public function down() {
-        $count = M('super_wave')->where("date='$this->today' and cur_per<=-0.2 and direction=1")->count();
+        $count = M('super_wave')->where("cur_per<=-0.2 and direction=1")->count();
         $page = new \Think\Page($count, PAGE_COUNT);
         $show = $page->show();
 
         $list = M('super_wave')
-            ->where("date='$this->today' and cur_per<-0.2 and direction=1")
+            ->where("cur_per<-0.2 and direction=1")
             ->join('left join stocks_info on super_wave.code = stocks_info.code')
-            ->field('super_wave.id, super_wave.code, name, industry, min, max, percent, cur_per')
+            ->field('super_wave.id, super_wave.code, date, man_date, name, industry, min, max, percent, cur_per, flag, cost_price, yield_rate')
             ->order('cur_per')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
@@ -53,14 +53,14 @@ class SuperController extends Controller {
     }
 
     public function super_up() {
-        $count = M('super_wave')->where("date='$this->today' and percent>=0.3 and direction=1")->count();
+        $count = M('super_wave')->where("direction=1")->count();
         $page = new \Think\Page($count, PAGE_COUNT);
         $show = $page->show();
 
         $list = M('super_wave')
-            ->where("date = '$this->today' and percent>=0.3 and direction=1")
+            ->where("direction=1")
             ->join('left join stocks_info on super_wave.code = stocks_info.code')
-            ->field('super_wave.id, super_wave.code, name, industry, min, max, percent, cur_per')
+            ->field('super_wave.id, super_wave.code, date, man_date, name, industry, min, max, percent, cur_per, flag, cost_price, yield_rate')
             ->order('percent desc')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
@@ -73,14 +73,14 @@ class SuperController extends Controller {
     }
 
     public function up() {
-        $count = M('super_wave')->where("date='$this->today' and cur_per>=0.2 and direction=-1")->count();
+        $count = M('super_wave')->where("cur_per>=0.2 and direction=-1")->count();
         $page = new \Think\Page($count, PAGE_COUNT);
         $show = $page->show();
 
         $list = M('super_wave')
-            ->where("date='$this->today' and cur_per>=0.2 and direction=-1")
+            ->where("cur_per>=0.2 and direction=-1")
             ->join('left join stocks_info on super_wave.code = stocks_info.code')
-            ->field('super_wave.id, super_wave.code, name, industry, min, max, percent, cur_per')
+            ->field('super_wave.id, super_wave.code, date, man_date, name, industry, min, max, percent, cur_per, flag, cost_price, yield_rate')
             ->order('cur_per desc')
             ->limit($page->firstRow.','.$page->listRows)
             ->select();
@@ -90,5 +90,20 @@ class SuperController extends Controller {
         $this->assign('page', $show);
 
         $this->display();
+    }
+
+    public function flag() {
+        $map['id'] = array('IN', I('get.ids'));
+
+        $data['man_date'] = $this->today;
+        $data['flag'] = 1;
+
+        $result = M('super_wave')->where($map)->save($data);
+
+        if($result) {
+            $this->success('操作成功！');
+        } else {
+            $this->error('操作失败！');
+        }
     }
 }
