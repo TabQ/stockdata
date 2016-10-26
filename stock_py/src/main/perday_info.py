02f9554477d6234c6816729d6ce23a51db23f0e1
TabQ <evenstar6@gmail.com>
2016-10-26 19:37:20 +0800

    为perday_info添加错误日志处理功能

--- stock_py/src/main/perday_info.py
+++ stock_py/src/main/perday_info.py
@@ -4,12 +4,13 @@ import MySQLdb
 from datetime import date
 import datetime
 import time
+import traceback
 
 print datetime.datetime.now()
 
 today = str(date.today())
 
-# today = '2016-07-19'
+# today = '2016-10-24'
 
 conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="stock", charset="utf8")
 cursor = conn.cursor()
@@ -38,53 +39,59 @@ if dateRes:
         maxVol = row[1]
         minPrice = row[2]
             
-        df = ts.get_hist_data(code, start=today, end=today)
-        if df is not None:
-            if today in df.index:
-                df_arr = df.ix[today]
-                
-                _open = df_arr['open']
-                high = df_arr['high']
-                close = df_arr['close']
-                low = df_arr['low']
-                volume = df_arr['volume']
-                price_change = df_arr['price_change']
-                p_change = df_arr['p_change']
-                ma5 = df_arr['ma5']
-                ma10 = df_arr['ma10']
-                ma20 = df_arr['ma20']
-                v_ma5 = df_arr['v_ma5']
-                v_ma10 = df_arr['v_ma10']
-                v_ma20 = df_arr['v_ma20']
-                turnover = df_arr['turnover']
-                
-                if ma20 != 0:
-                    p2ma20 = close / ma20
-                else:
-                    p2ma20 = 0
+        try:
+            df = ts.get_hist_data(code, start=today, end=today)
+            if df is not None:
+                if today in df.index:
+                    df_arr = df.ix[today]
                     
-                if minPrice != 0:
-                    p2min = close / minPrice
-                    ma20_2_min = ma20 / minPrice
-                else:
-                    p2min = ma20_2_min = 0
+                    _open = df_arr['open']
+                    high = df_arr['high']
+                    close = df_arr['close']
+                    low = df_arr['low']
+                    volume = df_arr['volume']
+                    price_change = df_arr['price_change']
+                    p_change = df_arr['p_change']
+                    ma5 = df_arr['ma5']
+                    ma10 = df_arr['ma10']
+                    ma20 = df_arr['ma20']
+                    v_ma5 = df_arr['v_ma5']
+                    v_ma10 = df_arr['v_ma10']
+                    v_ma20 = df_arr['v_ma20']
+                    turnover = df_arr['turnover']
                     
-                if v_ma20 != 0:
-                    v2ma20 = volume / v_ma20
-                else:
-                    v2ma20 = 0
+                    if ma20 != 0:
+                        p2ma20 = close / ma20
+                    else:
+                        p2ma20 = 0
+                        
+                    if minPrice != 0:
+                        p2min = close / minPrice
+                        ma20_2_min = ma20 / minPrice
+                    else:
+                        p2min = ma20_2_min = 0
+                        
+                    if v_ma20 != 0:
+                        v2ma20 = volume / v_ma20
+                    else:
+                        v2ma20 = 0
+                        
+                    if maxVol != 0:
+                        v2max = volume / maxVol
+                        vma20_2_max = v_ma20 / maxVol
+                    else:
+                        v2max = vma20_2_max = 0
                     
-                if maxVol != 0:
-                    v2max = volume / maxVol
-                    vma20_2_max = v_ma20 / maxVol
-                else:
-                    v2max = vma20_2_max = 0
-                
-                sql = "insert into perday_info(code,date,open,high,close,low,volume,price_change,p_change,ma5,ma10,ma20,v_ma5,v_ma10,v_ma20,turnover,\
-                p2ma20,p2min,ma20_2_min,v2ma20,v2max,vma20_2_max) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                param = (code,today,_open,high,close,low,volume,price_change,p_change,ma5,ma10,ma20,v_ma5,v_ma10,v_ma20,turnover,p2ma20,p2min,ma20_2_min,v2ma20,v2max,vma20_2_max)
-                cursor.execute(sql, param)
-                conn.commit()
+                    sql = "insert into perday_info(code,date,open,high,close,low,volume,price_change,p_change,ma5,ma10,ma20,v_ma5,v_ma10,v_ma20,turnover,\
+                    p2ma20,p2min,ma20_2_min,v2ma20,v2max,vma20_2_max) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                    param = (code,today,_open,high,close,low,volume,price_change,p_change,ma5,ma10,ma20,v_ma5,v_ma10,v_ma20,turnover,p2ma20,p2min,ma20_2_min,v2ma20,v2max,vma20_2_max)
+                    cursor.execute(sql, param)
+                    conn.commit()
+        except:
+            f=open("errors/"+today+".log",'a')
+            traceback.print_exc(file=f)
+            f.flush()
+            f.close()
                 
     # perday_info入库结束־
     now = time.time()
